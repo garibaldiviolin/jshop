c0c79b4221abc8d13c747a9635c729d79c94c32d
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-12 09:19:21 -0300

    start_script fixed to allow compilation and running

--- start_script.cmd
+++ start_script.cmd
@@ -1,2 +1,2 @@
-mvn clean package
-java -javaagent:springloaded.jar -noverify -cp target/jshop-debug.jar br.marketplace.App
+call mvn --batch-mode clean package
+java -javaagent:springloaded.jar -noverify -jar target/jshop-debug.jar
