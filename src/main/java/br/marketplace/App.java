d14e31382c2c58d312b2dd7a79e0a4953c844e27
garibaldiviolin <garibaldiviolin@gmail.com>
2018-09-12 09:18:23 -0300

    Spring Framework added to project

--- src/main/java/br/marketplace/App.java
+++ src/main/java/br/marketplace/App.java
@@ -1,13 +1,18 @@
 package br.marketplace;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
 }
