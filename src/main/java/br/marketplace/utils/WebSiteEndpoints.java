6c1a48a63f66fb277522dc22e56614f2db1d41da
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-05 12:46:16 -0300

    Update WebSiteEndpoints class

--- src/main/java/br/marketplace/utils/WebSiteEndpoints.java
+++ src/main/java/br/marketplace/utils/WebSiteEndpoints.java
@@ -5,9 +5,14 @@ public final class WebSiteEndpoints {
     public static final String BASE_URL = "/";
 
     public static final String INDEX = BASE_URL;
-    public static final String CATEGORY_ADD = "/add";
-    public static final String CATEGORY_EDIT = "/edit/{id}";
-    public static final String CATEGORY_DELETE = "/delete/{id}";
-    public static final String CATEGORY_SAVE = "/save";
+
+    public static final String MANAGER_INDEX = "/manager";
+
+    public static final String CATEGORY_INDEX = MANAGER_INDEX + "/category";
+
+    public static final String CATEGORY_ADD = CATEGORY_INDEX + "/add";
+    public static final String CATEGORY_EDIT = CATEGORY_INDEX + "/edit/{id}";
+    public static final String CATEGORY_DELETE = CATEGORY_INDEX + "/delete/{id}";
+    public static final String CATEGORY_SAVE = CATEGORY_INDEX + "/save";
 
 }
