eb51d2e2aad7c9d7da6169c42bc6338d519e272b
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-05 09:19:21 -0300

    Change controller directory structure

--- src/main/java/br/marketplace/controller/restapi/HelloController.java
+++ src/main/java/br/marketplace/controller/restapi/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello world!";
-    }
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello world!";
+	}
 
 }
