6f0e09ed8a2191cb564273d052d6f36a084935b1
garibaldiviolin <garibaldiviolin@gmail.com>
2018-10-05 10:55:37 -0300

    Add WebSiteEndpoints and RESTAPIEndpoints in utils package

---
+++ src/main/java/br/marketplace/utils/RESTAPIEndpoints.java
@@ -0,0 +1,9 @@
+package br.marketplace.utils;
+
+public final class RESTAPIEndpoints {
+
+    public static final String BASE_URL = "/api/v1";
+
+    public static final String HELLO = "/hello";
+
+}
---
+++ src/main/java/br/marketplace/utils/WebSiteEndpoints.java
@@ -0,0 +1,13 @@
+package br.marketplace.utils;
+
+public final class WebSiteEndpoints {
+
+    public static final String BASE_URL = "/";
+
+    public static final String INDEX = BASE_URL;
+    public static final String CATEGORY_ADD = "/add";
+    public static final String CATEGORY_EDIT = "/edit/{id}";
+    public static final String CATEGORY_DELETE = "/delete/{id}";
+    public static final String CATEGORY_SAVE = "/save";
+
+}
